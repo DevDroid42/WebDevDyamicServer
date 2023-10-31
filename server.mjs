38d3459deb575845460d68651307366bd39e0cde
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-10-31 11:33:31 -0500

    basic database functionality added

--- server.mjs
+++ server.mjs
@@ -4,14 +4,13 @@ import * as url from 'node:url';
 
 import { default as express } from 'express';
 import { default as sqlite3 } from 'sqlite3';
+import { rejects } from 'node:assert';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const port = 8000;
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const root = path.join(__dirname, 'public');
 const template = path.join(__dirname, 'templates');
-
-let app = express();
-app.use(express.static(root));
 
 const db = new sqlite3.Database(path.join(__dirname, 'urbanization.sqlite3'), sqlite3.OPEN_READONLY, (err) => {
     if (err) {
@@ -22,63 +21,38 @@ const db = new sqlite3.Database(path.join(__dirname, 'urbanization.sqlite3'), sq
     }
 });
 
-app.get('/mfr/:name', (req, res) => {
-    let manufacturer = req.params.name;
-    console.log(manufacturer);
 
-    let data1 = null;
-    let data2 = null;
-    let finishAndSend = function() {
-        fs.readFile(path.join(template, 'mfr.html'), 'utf-8', (err, data) => {
-            let response = data.replace('$$MFR_NAME$$', data2.name);
-            let table_body = '';
-            data1.forEach((cereal) => {
-                let table_row = '<tr>';
-                table_row += '<td>' + cereal.name + '</td>';
-                table_row += '<td>' + cereal.type + '</td>';
-                table_row += '<td>' + cereal.calories + '</td>';
-                table_row += '<td>' + cereal.fat + '</td>';
-                table_row += '<td>' + cereal.protein + '</td>';
-                table_row += '<td>' + cereal.carbohydrates + '</td>';
-                table_row += '</tr>\n';
-                table_body += table_row;
-            });
-            response = response.replace('$$TABLE_BODY$$', table_body);
-            res.status(200).type('html').send(response);
+function queryDatabase(query, params){
+    return new Promise((Resolve, Reject) =>{
+        db.all(query, params, (err, rows) =>{
+            if(err){
+                Reject(err);
+            }else{
+                Resolve(rows);
+            }
         });
-    };
-
-    let query1 = 'SELECT * FROM Cereals WHERE mfr = ?';
-    db.all(query1, [manufacturer.toUpperCase()], (err, rows) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            data1 = rows;
-            if (data1 !== null && data2 !== null) {
-                finishAndSend();
-            }
-        }
     });
-    let query2 = 'SELECT * FROM Manufacturers WHERE id = ?';
-    db.get(query2, [manufacturer.toUpperCase()], (err, row) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            data2 = row;
-            if (data1 !== null && data2 !== null) {
-                finishAndSend();
-            }
-        }
-    });
+}
 
-    // let num2 = parseInt(req.params.num2);
-    // let answer = num1 + num2;
-    // fs.readFile(path.join(template, 'add.html'), 'utf-8', (err, data) => {
-    //     let response = data.replace('$$EQUATION$$', num1 + ' + ' + num2 + ' = ' + answer);
-    //     res.status(200).type('html').send(response);
-    // });
+function getTemplate(templateFile){
+    return new Promise((Resolve, Reject) =>{
+        fs.readFile(path.join(template, templateFile), 'utf-8', (err, data) => {
+            if(err){
+                Reject(err);
+            }else{
+                Resolve(data);
+            }
+        });
+    });
+}
+
+let app = express();
+app.use(express.static(root));
+
+app.get('/', (req, res) => {
+    Promise.all([getTemplate('dynamicTemp1.html'), queryDatabase("SELECT * FROM Urbanization")]).then(values=>{
+        res.status(200).type('text').send(values[0] + values[1]);
+    });
 });
 
 app.listen(port, () => {
